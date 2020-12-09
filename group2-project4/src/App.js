ef02ccfeb9dcbf462c6e82e98309c1f1429ba7f6
Konrad W <kornisemail@gmail.com>
2020-12-09 09:53:25 +0100

    created landingpage component

--- group2-project4/src/App.js
+++ group2-project4/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-    
-      </header>
+      <LandingPage />
     </div>
   );
 }
