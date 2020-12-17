ed33e20cd4ebf3512e82b0c71355a69f3280e5f2
rawadsalman <rwadsalman81@gmail.com>
2020-12-17 17:12:51 +0100

    adding books info

--- group2-project4/src/components/InfoContainer.js
+++ group2-project4/src/components/InfoContainer.js
@@ -16,13 +16,25 @@ book.fields.rating - number from 1-5 - we can display this amount of stars
 function InfoContainer({ book }) {
   return (
     <div className="info-container__container">
+
       <div className="info-container">
-        {book.fields.bookInfo.title}
-        <img src={book.fields.bookInfo.thumbnail} />
-        <h2>Review</h2>
-        <div>REVIEW TEXT</div>
+      
+         <p>
+            <h2 className="Book-title">{book.fields.bookInfo.title}</h2> 
+            {  /* <h2>{book.fields.bookInfo.thumbnail}</h2>  */}
+            <hr className="hr"/>
+            <br/>
+            <h2 className="info">Authers: {book.fields.bookInfo.authors}</h2>
+            <h2 className="info">categories: {book.fields.bookInfo.categories}</h2>  {/* (like science fiction, ...) - is not provided all the time*/}
+            <h2 className="info">Date: {book.fields.bookInfo.date}</h2> {/* date of publish */}
+            <h2 className="info">Pages:{book.fields.bookInfo.pages}</h2>
+            <h2 className="info">Review:{book.fields.bookInfo.review}</h2>
+            {book.fields.bookInfo.review && <h2 className="info">Review:{book.fields.bookInfo.review}</h2>}
+            <h2 className="info">Rating:{book.fields.rating}</h2>
+        </p>
       </div>
-    </div>
+      </div>
+
   );
 }
 
