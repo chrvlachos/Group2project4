5fb67e11c77e841b3056e01533ac6d45bef4f597
Chris Vlachos <chrvlachos@gmail.com>
2020-12-18 10:46:13 +0100

    added new styling for info button

--- group2-project4/src/components/InfoContainer.js
+++ group2-project4/src/components/InfoContainer.js
@@ -13,28 +13,27 @@ book.fields.review - displays our review
 book.fields.rating - number from 1-5 - we can display this amount of stars
 */
 
-function InfoContainer({ book }) {
+function InfoContainer({ book, setShowInfos }) {
+  
   return (
-    <div className="info-container__container">
-
       <div className="info-container">
       
          <p>
-            <h2 className="Book-title">{book.fields.bookInfo.title}</h2> 
+           <div className="align-title">
+               <h2 className="Book-title">{book.fields.bookInfo.title}</h2> 
+               <button onClick={()=> setShowInfos(-1)} className="Closing-button">Close</button>
+           </div>
             {  /* <h2>{book.fields.bookInfo.thumbnail}</h2>  */}
             <hr className="hr"/>
             <br/>
-            <h2 className="info">Authers: {book.fields.bookInfo.authors}</h2>
-            <h2 className="info">categories: {book.fields.bookInfo.categories}</h2>  {/* (like science fiction, ...) - is not provided all the time*/}
+            <h2 className="info">Author: {book.fields.bookInfo.authors}</h2>
+            <h2 className="info">Category: {book.fields.bookInfo.categories}</h2>  {/* (like science fiction, ...) - is not provided all the time*/}
             <h2 className="info">Date: {book.fields.bookInfo.date}</h2> {/* date of publish */}
             <h2 className="info">Pages:{book.fields.bookInfo.pages}</h2>
-            <h2 className="info">Review:{book.fields.bookInfo.review}</h2>
-            {book.fields.bookInfo.review && <h2 className="info">Review:{book.fields.bookInfo.review}</h2>}
             <h2 className="info">Rating:{book.fields.rating}</h2>
+            <h2 className="info">Review: {book.fields.review.content[0].content[0].value} </h2>
         </p>
       </div>
-      </div>
-
   );
 }
 
